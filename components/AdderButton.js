a6594e03ff20b06696a91f29709904eda25d5b69
Saeed <soltanisaeed1996@gmail.com>
2021-01-03 02:30:19 +0330

    add adderButton component

---
+++ components/AdderButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+
+const AdderButton = () => {
+    return(
+        <View>
+            <Button>
+
+            </Button>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    constainer:{},
+})
+export default AdderButton;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,17 @@
-import React,{useState} from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 const HomeScreen = () => {
 
-    const [drink,setDrink] = useState(0);
+    const [drink, setDrink] = useState(0);
     return (
         <View style={styles.container} >
             <View style={styles.header} >
-                <Text>header</Text>
+                <Text style={styles.headerText} >WaterApp</Text>
             </View>
             <View style={styles.drinkLogger} >
-                <Text>drink = {drink}</Text>
+                <Text style={styles.drinkLoggerText} >{drink}</Text>
             </View>
             <View style={styles.adder} >
                 <Text>press!</Text>
@@ -21,22 +21,36 @@ const HomeScreen = () => {
 }
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        justifyContent:'flex-start',
-        alignItems:'flex-start',
-        backgroundColor:'#FFFFFF',
-        flexDirection:'column',
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'column',
     },
-    header : {
-        flex:1,
-        backgroundColor:'#FFFFFF',
+    header: {
+        flex: 1,
+        backgroundColor: '#ECEBE9',
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+    headerText: {
+        color: "#006EE6",
+        fontSize:25,
+    },
+    drinkLogger: {
+        flex: 4,
+        backgroundColor: '#F6F5F4',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    drinkLoggerText: {
+        fontSize: 150,
+        color: '#006EE6',
+    },
+    adder: {
+        flex: 4,
+        backgroundColor: '#F6F5F4',
 
-    },
-    drinkLogger : {
-        flex:4
-    },
-    adder:{
-        flex:4
     },
 })
 export default HomeScreen;
